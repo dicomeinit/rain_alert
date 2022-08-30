30b4823bf3231ceb2b07052a40ad371910961086
Diana <dicomeinit@gmail.com>
2022-08-31 00:39:48 +0300

    Check if it Will Rain in the Next 12 Hours

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ weather_params = {
     "lat": 50.447731,
     "lon": 30.542721,
     "appid": api_key,
+    "exclude": "current,minutely,daily",
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
 
-print(response.status_code)
-print(response.json())
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
+# print(weather_data["hourly"][0]["weather"][0]["id"])
